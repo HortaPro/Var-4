16cb672cc5e2c386544308cce252516d85ec1b8f
Horta <Horta@DESKTOP-DQM7GVE>
2022-09-14 17:36:20 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Double a, t, D;
+Console.Write("Введите a:");
+a = int.Parse(Console.ReadLine());
+Console.Write("Ввелите t:");
+t = int.Parse(Console.ReadLine());
+D = 9.8 * (a * a) + 5.52 * Math.Cos(t * t * t * t * t);
+Console.WriteLine($"{D:F2}");
